4176e106d4c72c8ef21067bc60e170f19153b00d
yukisama1 <152201388+yukisama1@users.noreply.github.com>
2024-10-13 18:39:43 +0200

    ja kp digga was n arsch is wieder ad

--- AD/Uebung 1/Loseung Marc/U1_4.java
+++ AD/Uebung 1/Loseung Marc/U1_4.java
@@ -1,17 +1,18 @@
 import java.io.*;
 import java.util.*;
+import java.time.*;
 
 
 public class U1_4 {
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(3, 2);
+        Matrix matrix = new Matrix(2600, 2600);
         matrix.init();
         //matrix.input();
         matrix.randomfill();
-        matrix.print();
-        Matrix secmatrix = new Matrix(2, 4);
+        //matrix.print();
+        Matrix secmatrix = new Matrix(2600, 2600);
         secmatrix.randomfill();
-        secmatrix.print();
+        //secmatrix.print();
         //Matrix summatrix = matrix.add(secmatrix);
         Matrix productmatrix = matrix.mult(secmatrix);
     }
@@ -66,7 +67,8 @@ class Matrix {
     }
 
     Matrix add(Matrix secmatrix) {
-        int opcount = 0;
+        long start = System.currentTimeMillis();
+        double opcount = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 secmatrix.setvalue(i, j, matrix[i][j] + secmatrix.getvalue(i, j));
@@ -75,15 +77,18 @@ class Matrix {
             }
             opcount++;
         }
-        secmatrix.print();
+        //secmatrix.print();
         opcount++;
         System.out.println("Anzahl Aktionen bei add: " + opcount);
+        long stop = System.currentTimeMillis();
+        System.out.println("Runtime in ms: " + (stop-start) + " oder in s: " + (stop-start)/1000);
         return secmatrix;
     }
 
     Matrix mult(Matrix secmatrix) {
+        long start = System.currentTimeMillis();
         Matrix tempmatrix = new Matrix(m, secmatrix.n);      
-        int opcount = 0;    
+        double opcount = 0;    
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < secmatrix.n; j++) {
                 int temp = 0;
@@ -98,9 +103,11 @@ class Matrix {
             }
             opcount++;
         }
-        tempmatrix.print();
+        //tempmatrix.print();
         opcount++;
         System.out.println("Anzahl Aktionen bei mult: " + opcount);
+        long stop = System.currentTimeMillis();
+        System.out.println("Runtime in ms: " + (stop-start) + " oder in s: " + (stop-start)/1000);
         return tempmatrix;
     }
 

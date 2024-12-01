ddfd9cade66e1a82a31ab6d0e9d5d52f7d108423
loh35595 <hermann.loeffelmann@st.oth-regensburg.de>
2024-12-01 11:07:07 +0100

    added solution about the amount of columns in Abgabe 8

--- DB/Uebung 8/Loesung Hermann/Abgabe 8.sql
+++ DB/Uebung 8/Loesung Hermann/Abgabe 8.sql
@@ -4,3 +4,5 @@ create view hbf as
 select b.bf_nr, b.bundesland, b.station, b.kat_vst as kategorie
 from bahnhoefe b 
 where b.station like '%Hbf' or b.station like '%Hauptbahnhof';
+
+--Es sind 132 Zeilen

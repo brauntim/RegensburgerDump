1ea6944297f11788855e0417ec4768060f55e8ec
loh35595 <hermann.loeffelmann@st.oth-regensburg.de>
2024-12-02 18:45:52 +0100

    added almost full solution of Uebung 8

--- DB/Uebung 8/Loesung Hermann/Übung 8.sql
+++ DB/Uebung 8/Loesung Hermann/Übung 8.sql
@@ -38,3 +38,74 @@ insert into grosser_bhf (station, bundesland, bf_nr, kategorie) values
 --g)
 select * from grosser_bhf gb 
 where gb.bf_nr = 9999; --Nein
+
+--h)
+drop view grosser_bhf;
+
+create view grosser_bhf as
+select *
+from hbf h 
+where h.kategorie = 1
+with cascaded check option;
+
+
+--3.)
+create materialized view Bahnhof_Statistik as
+select b.bundesland, count(station) as anzahl_bahnhoefe
+from bahnhoefe b
+group by b.bundesland;
+
+select * from bahnhof_statistik;
+
+
+--4.)
+select * from bahnhoefe b;
+--a)
+select distinct bundesland
+from bahnhoefe b;
+
+--b)
+select b.bm, count(station) as anzahl_bahnhoefe
+from bahnhoefe b
+group by b.bm
+having count(station) > 10;
+
+--c)
+select b2.station, b2.kat_vst 
+from bahnhoefe b 
+join bahnhoefe b2 on b.station = 'Ulm Hbf'
+where b2.kat_vst = b.kat_vst;
+
+--or more logically
+select b.station, b.kat_vst
+from bahnhoefe b 
+join bahnhoefe b2 on b.kat_vst = b2.kat_vst
+where b2.station = 'Ulm Hbf';
+
+--d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
